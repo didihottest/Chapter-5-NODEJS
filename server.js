af082c8c0ad4977544827c6fbe414c49e5e6c8df
didihottest <didihottest@gmail.com>
2021-03-15 11:20:13 +0700

    ejs working

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const app = express();
+const fs = require("fs")
 app.use(express.static(__dirname));
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-   res.sendFile(__dirname + "/index.html");
+   res.render("index");
 });
 
+app.get("/login", function(req, res){
+    res.render("login");
+})
+
+app.post("/login", function(req, res){
+    let playerName = {playerName:req.body.playerName};
+    playerNameJSON = JSON.stringify(playerName);
+    fs.writeFile("json/playerName.json", playerNameJSON, (err)=>err)
+    res.redirect('/game');
+})
+
 app.get("/game", function(req, res){
-    res.sendFile(__dirname + "/game.html");
+    fs.readFile('json/playerName.json', (err, data) => {
+        if (err) throw err;
+        let playerLoggedin = JSON.parse((data.toString()))
+        res.render("game", {playerName:playerLoggedin.playerName});
+      });
 });
 
 app.listen(3000, function(){
